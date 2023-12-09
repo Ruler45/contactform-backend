a27d5a258ad5edb381c78cc5c1026b652cbe9a0b
Ruler45 <sahin0945@gmail.com>
2023-12-09 11:30:34 +0530

    new update

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/socials', (req, res) => {
+  res.send({
+    website: "https://sahin-alam-portfolio.netlify.app",
+    github: "https://github.com/Ruler45"
+  })
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
