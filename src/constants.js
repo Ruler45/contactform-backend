a262464441fc6fb545b4288aa33267481eaf4a31
ruler45 <sahin0945@gmail.com>
2023-12-16 11:08:05 +0530

    connection issues fixed

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
 export const DB_NAME="Contact"
+export const MONGODB_URI="mongodb+srv://ruler45:sahinalam1@learncluster.rvxyvio.mongodb.net"
--- src/db/db.js
+++ src/db/db.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
+import { DB_NAME , MONGODB_URI } from "../constants.js";
 
 const connectDB = async () => {
     try {
-       const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       const connectionInstance= await mongoose.connect(`${MONGODB_URI}/${DB_NAME}`)
     
-         console.log(`\n MongoDB connected: ${connectionInstance.connection.host}`);
+         console.log(`\nMongoDB connected: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MongoDB connection failed.",error.message);
         process.exit(1);
